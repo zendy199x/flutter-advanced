6c0568a053db998c5f3f3511132e2d002c00aa9b
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-05-17 00:08:48 +0700

    using private properties

--- flutter_basics/flutter_complete_guide/lib/main.dart
+++ flutter_basics/flutter_complete_guide/lib/main.dart
@@ -6,39 +6,55 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  void answerQuestion() {
-    print("Answer chosen");
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+    print(_questionIndex);
   }
 
   @override
   Widget build(BuildContext context) {
     var questions = [
       'What\'s your favorite color?',
-      'What\'s your favorite animarl?'
+      'What\'s your favorite animal?',
     ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("My First App"),
+          title: Text('My First App'),
         ),
         body: Column(
           children: [
-            Text("The question!"),
-            RaisedButton(
-              child: Text("Answer 1"),
-              onPressed: answerQuestion,
+            Text(
+              questions[_questionIndex],
             ),
             RaisedButton(
-              child: Text("Answer 2"),
-              onPressed: () => print("Answer 2 chosen"),
+              child: Text('Answer 1'),
+              onPressed: _answerQuestion,
             ),
             RaisedButton(
-              child: Text("Answer 3"),
+              child: Text('Answer 2'),
+              onPressed: () => print('Answer 2 chosen!'),
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
               onPressed: () {
-                print("Answer 3 chosen");
+                // ...
+                print('Answer 3 chosen');
               },
-            )
+            ),
           ],
         ),
       ),
