1fc3a0e6375f3fbf1e3611383df8455f6a263acd
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-05-16 23:15:36 +0700

    anonymous function

--- flutter_basics/flutter_complete_guide/lib/main.dart
+++ flutter_basics/flutter_complete_guide/lib/main.dart
@@ -31,11 +31,13 @@ class MyApp extends StatelessWidget {
             ),
             RaisedButton(
               child: Text("Answer 2"),
-              onPressed: answerQuestion,
+              onPressed: () => print("Answer 2 chosen"),
             ),
             RaisedButton(
               child: Text("Answer 3"),
-              onPressed: answerQuestion,
+              onPressed: () {
+                print("Answer 3 chosen");
+              },
             )
           ],
         ),
