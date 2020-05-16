2cfb17100d8288d877bbf7e14c65700e21ed4312
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-05-17 03:30:14 +0700

    splitting the app into widgets

--- flutter_basics/flutter_complete_guide/lib/main.dart
+++ flutter_basics/flutter_complete_guide/lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 
-import './question.dart';
-import './answer.dart';
-
+import './quiz.dart';
+import './result.dart';
 // void main() {
 //   runApp(MyApp());
 // }
@@ -17,60 +16,57 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  final questions = const [
+  final _questions = const [
     {
       'questionText': 'What\'s your favorite color?',
-      'answer': ['Black', 'Red', 'Green', 'White'],
+      'answers': ['Black', 'Red', 'Green', 'White'],
     },
     {
       'questionText': 'What\'s your favorite animal?',
-      'answer': ['Rabbit', 'Snake', 'Elephant', 'Lion'],
+      'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion'],
     },
     {
       'questionText': 'Who\'s your favorite instructor?',
-      'answer': ['Zendy', 'Cheng', 'Min', 'Justin'],
-    }
+      'answers': ['Max', 'Max', 'Max', 'Max'],
+    },
   ];
-
   var _questionIndex = 0;
 
   void _answerQuestion() {
-      setState(() {
-        _questionIndex = _questionIndex + 1;
-      });
+    // var aBool = true;
+    // aBool = false;
+
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
     print(_questionIndex);
-    if (_questionIndex < questions.length) {
-      print("We have more questions");
+    if (_questionIndex < _questions.length) {
+      print('We have more questions!');
     } else {
-      print("No more question");
+      print('No more questions!');
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    // var dummy = ["Hello"];
-    // dummy.add("Max");
+    // var dummy = const ['Hello'];
+    // dummy.add('Max');
     // print(dummy);
+    // dummy = [];
+    // questions = []; // does not work if questions is a const
 
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: _questionIndex < questions.length ? 
-        Column(
-          children: [
-            Question(
-              questions[_questionIndex]['questionText'],
-            ),
-            ...(questions[_questionIndex]['answer'] as List<String>)
-                .map((answer) {
-              return Answer(_answerQuestion, answer);
-            }).toList()
-          ],
-        ) : Center(
-          child: Text("You did it"),
-        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
+              )
+            : Result(),
       ),
     );
   }
