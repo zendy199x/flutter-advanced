8deb1ee7aca0f6b715f50f64d16828fe8297400f
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-05-16 22:18:49 +0700

    add layout widgets

--- flutter_basics/flutter_complete_guide/lib/main.dart
+++ flutter_basics/flutter_complete_guide/lib/main.dart
@@ -7,14 +7,33 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animarl?'
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("My First App"),
         ),
         body: Column(
-          children: <Widget>[],
+          children: [
+            Text("The question!"),
+            RaisedButton(
+              child: Text("Answer 1"),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text("Answer 2"),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text("Answer 3"),
+              onPressed: null,
+            )
+          ],
         ),
       ),
     );
