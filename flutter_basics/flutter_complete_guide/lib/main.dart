94c14655ca9a24fba87ddc20cf42d300c87ca85b
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-05-16 23:11:00 +0700

    connecting function & buttons

--- flutter_basics/flutter_complete_guide/lib/main.dart
+++ flutter_basics/flutter_complete_guide/lib/main.dart
@@ -7,6 +7,10 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print("Answer chosen");
+  }
+
   @override
   Widget build(BuildContext context) {
     var questions = [
@@ -23,15 +27,15 @@ class MyApp extends StatelessWidget {
             Text("The question!"),
             RaisedButton(
               child: Text("Answer 1"),
-              onPressed: null,
+              onPressed: answerQuestion,
             ),
             RaisedButton(
               child: Text("Answer 2"),
-              onPressed: null,
+              onPressed: answerQuestion,
             ),
             RaisedButton(
               child: Text("Answer 3"),
-              onPressed: null,
+              onPressed: answerQuestion,
             )
           ],
         ),
