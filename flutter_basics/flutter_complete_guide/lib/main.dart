f80282423b84123b694affec00475b339a4dacd3
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-05-17 04:01:24 +0700

    reset the quiz

--- flutter_basics/flutter_complete_guide/lib/main.dart
+++ flutter_basics/flutter_complete_guide/lib/main.dart
@@ -48,6 +48,13 @@ class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
   var _totalScore = 0;
 
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
   void _answerQuestion(int score) {
     // var aBool = true;
     // aBool = false;
@@ -84,7 +91,7 @@ class _MyAppState extends State<MyApp> {
                 questionIndex: _questionIndex,
                 questions: _questions,
               )
-            : Result(_totalScore),
+            : Result(_totalScore, _resetQuiz),
       ),
     );
   }
