d3dfb43497df56e28f37b9bb2c51b06d4b0d0fd7
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-05-20 00:05:44 +0700

    populationg the chart with values

--- widgets_styling_logic/personal_expenses_app/lib/widgets/chart.dart
+++ widgets_styling_logic/personal_expenses_app/lib/widgets/chart.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import "package:intl/intl.dart";
 
+import './chart_bar.dart';
 import '../models/transaction.dart';
 
 class Chart extends StatelessWidget {
@@ -33,6 +34,12 @@ class Chart extends StatelessWidget {
     });
   }
 
+  double get totalSpending {
+    return groupedTransactionValues.fold(0.0, (sum, item) {
+      return sum + item["amount"];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     print(groupedTransactionValues);
@@ -42,7 +49,12 @@ class Chart extends StatelessWidget {
       margin: EdgeInsets.all(20),
       child: Row(
         children: groupedTransactionValues.map((data) {
-          return Text("${data["day"]}: ${data["amount"]}");
+          return ChartBar(
+              data["day"],
+              data["amount"],
+              totalSpending == 0.0
+                  ? 0.0
+                  : (data["amount"] as double) / totalSpending);
         }).toList(),
       ),
     );
