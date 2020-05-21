4a05d8b3f4ef5e02fb0cb7e4848cfb0f78e77a1e
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-05-21 22:04:15 +0700

    registering a screen as the Main Screen

--- navigation_and_multiple_screens/meals_app/lib/dummy_data.dart
+++ navigation_and_multiple_screens/meals_app/lib/dummy_data.dart
@@ -1,4 +1,5 @@
-import "package:flutter/material.dart";
+import 'package:flutter/material.dart';
+
 import './models/category.dart';
 
 const DUMMY_CATEGORIES = const [
--- navigation_and_multiple_screens/meals_app/lib/main.dart
+++ navigation_and_multiple_screens/meals_app/lib/main.dart
@@ -1,4 +1,6 @@
-import "package:flutter/material.dart";
+import 'package:flutter/material.dart';
+
+import './categories_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,30 +8,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "DeliMeals",
+      title: 'DeliMeals',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("DeliMeals"),
-      ),
-      body: Center(
-        child: Text("Navigation Time"),
-      ),
+      home: CategoriesScreen(),
     );
   }
 }
