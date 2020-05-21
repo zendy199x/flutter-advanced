f80eaa16f02336e47548afda4ccc6e9caebdf46e
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-05-21 22:07:55 +0700

    passing data via the constructor

--- navigation_and_multiple_screens/meals_app/lib/categories_screen.dart
+++ navigation_and_multiple_screens/meals_app/lib/categories_screen.dart
@@ -15,9 +15,10 @@ class CategoriesScreen extends StatelessWidget {
         children: DUMMY_CATEGORIES
             .map(
               (catData) => CategoryItem(
-                    catData.title,
-                    catData.color,
-                  ),
+                catData.id,
+                catData.title,
+                catData.color,
+              ),
             )
             .toList(),
         gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
