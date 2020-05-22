92866f9b61250ffb1360c863b0ac03d5e0a32f12
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-05-22 20:30:59 +0700

    defining a data model

---
+++ state_management/shop_app/lib/screens/products_overview_screen.dart
@@ -0,0 +1,12 @@
+import "package:flutter/material.dart";
+
+class ProductsOverviewScreenn extends StatelessWidget {
+  const ProductsOverviewScreenn({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
