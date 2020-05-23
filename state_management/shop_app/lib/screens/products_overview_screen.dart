510fb029f1b518e7e0a614a795beaf837edba564
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-05-24 03:49:16 +0700

    using nested Models & Providers

--- state_management/shop_app/lib/screens/products_overview_screen.dart
+++ state_management/shop_app/lib/screens/products_overview_screen.dart
@@ -1,6 +1,6 @@
 import "package:flutter/material.dart";
 
-import "../models/product.dart";
+import "../providers/product.dart";
 import "../widgets/product_item.dart";
 import "../widgets/products_grid.dart";
 
