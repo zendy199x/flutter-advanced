c24b53455ccf0aeb2dc017d24697b4ee3319d4fe
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-05-25 03:02:06 +0700

    working with Multiple Providers

--- state_management/shop_app/lib/main.dart
+++ state_management/shop_app/lib/main.dart
@@ -4,6 +4,7 @@ import "package:provider/provider.dart";
 import "./screens/products_overview_screen.dart";
 import "./screens/product_detail_screen.dart";
 import "./providers/products.dart";
+import "./providers/cart.dart";
 
 void main() => runApp(MyApp());
 
@@ -11,20 +12,28 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // return ChangeNotifierProvider(
-    return ChangeNotifierProvider.value(
-      // create: (_) => Products(), // using builder if version provider < 4.0
-      value: Products(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(
+          // create: (_) => Products(), // using builder if version provider < 4.0
+          value: Products(),
+        ),
+        ChangeNotifierProvider.value(
+          value: Cart(),
+        ),
+      ],
       child: MaterialApp(
-          title: "MyShop",
-          theme: ThemeData(
-            primaryColor: Colors.purple,
-            accentColor: Colors.deepOrange,
-            fontFamily: "Lato",
-          ),
-          home: ProductsOverviewScreen(),
-          routes: {
-            ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
-          }),
+        title: "MyShop",
+        theme: ThemeData(
+          primaryColor: Colors.purple,
+          accentColor: Colors.deepOrange,
+          fontFamily: "Lato",
+        ),
+        home: ProductsOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+        },
+      ),
     );
   }
 }
--- state_management/shop_app/lib/providers/products.dart
+++ state_management/shop_app/lib/providers/products.dart
@@ -38,12 +38,7 @@ class Products with ChangeNotifier {
     ),
   ];
 
-  // var _showFavoritesOnly = false;
-
   List<Product> get items {
-    // if (_showFavoritesOnly) {
-    //   return _items.where((prodItem) => prodItem.isFavorite).toList();
-    // }
     return [..._items];
   }
 
@@ -54,16 +49,6 @@ class Products with ChangeNotifier {
   Product findById(String id) {
     return _items.firstWhere((prod) => prod.id == id);
   }
-
-  // void showFavoritesOnly() {
-  //   _showFavoritesOnly = true;
-  //   notifyListeners();
-  // }
-
-  // void showAll() {
-  //   _showFavoritesOnly = false;
-  //   notifyListeners();
-  // }
 
   void addProduct() {
     notifyListeners();
