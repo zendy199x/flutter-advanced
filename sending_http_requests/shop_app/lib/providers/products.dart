df543be289958098627e3ee5c577deaa5b88e4af
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-05-26 23:00:38 +0700

    working with flutters in dart

--- sending_http_requests/shop_app/lib/providers/products.dart
+++ sending_http_requests/shop_app/lib/providers/products.dart
@@ -69,31 +69,32 @@ class Products with ChangeNotifier {
 
   void addProduct(Product product) {
     const url = "https://flutter-update-2f80f.firebaseio.com/products.json";
-    http.post(
-      url,
-      body: json.encode(
-        {
-          "title": product.title,
-          "description": product.description,
-          "imageUrl": product.imageUrl,
-          "price": product.price,
-          "isFavotire": product.isFavorite,
-        },
-      ),
-    );
     // JSON: Javascript Object Notation;
+    http
+        .post(
+      url,
+      body: json.encode({
+        "title": product.title,
+        "description": product.description,
+        "imageUrl": product.imageUrl,
+        "price": product.price,
+        "isFavotire": product.isFavorite,
+      }),
+    )
+        .then((response) {
+          print(json.decode(response.body));
+      final newProduct = Product(
+        title: product.title,
+        description: product.description,
+        price: product.price,
+        imageUrl: product.imageUrl,
+        id: json.decode(response.body)["name"],
+      );
 
-    final newProduct = Product(
-      title: product.title,
-      description: product.description,
-      price: product.price,
-      imageUrl: product.imageUrl,
-      id: DateTime.now().toString(),
-    );
-
-    _items.add(newProduct);
-    // _items.insert(0, newProduct); // at the start of the list
-    notifyListeners();
+      _items.add(newProduct);
+      // _items.insert(0, newProduct); // at the start of the list
+      notifyListeners();
+    });
   }
 
   void updateProduct(String id, Product newProduct) {
