141936fc3d8c85a71d5b79b77249f8e4a0cdb3a9
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-05-26 22:43:33 +0700

    edit product screen and A problem

---
+++ working_with_user_input_and_forms/shop_app/lib/screens/edit_product_screen.dart
@@ -0,0 +1,19 @@
+import "package:flutter/material.dart";
+
+class EditProductScreen extends StatefulWidget {
+  EditProductScreen({Key key}) : super(key: key);
+
+  @override
+  _EditProductScreenState createState() => _EditProductScreenState();
+}
+
+class _EditProductScreenState extends State<EditProductScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Edit Product"),
+      ),
+    );
+  }
+}
