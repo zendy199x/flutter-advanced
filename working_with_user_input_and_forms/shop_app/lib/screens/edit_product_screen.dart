683568cec6322f2e5170115c6347b4985a1a62b2
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-05-26 22:44:23 +0700

    using forms and working with form inputs

--- working_with_user_input_and_forms/shop_app/lib/screens/edit_product_screen.dart
+++ working_with_user_input_and_forms/shop_app/lib/screens/edit_product_screen.dart
@@ -1,7 +1,7 @@
 import "package:flutter/material.dart";
 
 class EditProductScreen extends StatefulWidget {
-  EditProductScreen({Key key}) : super(key: key);
+  static const routeName = "/edit-product";
 
   @override
   _EditProductScreenState createState() => _EditProductScreenState();
@@ -13,6 +13,21 @@ class _EditProductScreenState extends State<EditProductScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text("Edit Product"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          child: ListView(
+            children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: "Title",
+                ),
+                textInputAction: TextInputAction.next,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
